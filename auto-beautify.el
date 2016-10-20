ffd5a183558a638d3a6b4231cbdb2c0b2ceda80a
quanwei <quanwei9958@126.com>
2016-10-20 10:42:37 +0800

    fix format

--- auto-beautify.el
+++ auto-beautify.el
@@ -1,10 +1,24 @@
 ;;; auto-beautify --- auto format you js/jsx file
 
 ;; Author: quanwei9958@126.com
-;; Version: 0.0.1
-;; Package-Requires: ((web-beautify))
+;; Version: 0.0.2
+;; Package-Requires: ((web-beautify "0.3.1"))
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Usage:
+;; (add-hook 'js2-mode-hook 'auto-beautify-mode)
 ;;; Code:
 
 (require 'web-beautify)
@@ -18,7 +32,7 @@
     (web-beautify-format-region web-beautify-js-program (line-beginning-position) (line-end-position))))
 
 (defun auto-beautify-when-branck ()
-  "eval web-beautify when region"
+  "eval web-beautify when branck"
   (interactive)
   (insert-char 125)
   (save-excursion
@@ -46,4 +60,4 @@
 (provide 'auto-beautify)
 
 
-;;; end of auto-beautify
+;;; auto-beautify.el ends here
